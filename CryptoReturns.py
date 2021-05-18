13d49ea65940ed3e06654c48522b8ce493841149
Willie David Jones III <Williejones005@gmail.com>
2021-05-18 12:05:06 -0700

    Added color code for multiplier and new gif to reflect that change

--- CryptoReturns.py
+++ CryptoReturns.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask import redirect
 from flask import render_template
 from flask import request
+from flask import flash
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -20,6 +21,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'something only you know'
 
 db = SQLAlchemy(app)
 
@@ -59,7 +61,7 @@ def home():
             twenty_four_hour_change = cg.get_price(ids=[coin_name.lower()], vs_currencies='usd', include_24hr_change='true')[coin_name.lower()]['usd_24h_change']
             ath = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
             ath_change_percentage = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
-            coin_gecko_sparkline = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='true')[0]
+            #coin_gecko_sparkline = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='true')[0]
 
             coin = Coin(
                         crypto_name=coin_gecko_name['name'],
@@ -80,7 +82,10 @@ def home():
             db.session.rollback()
             print('Failed to add coin')
             print(e)
+            print(coin_name)
+            flash(f"Could not find {coin_name}. Please enter {coin_name} full name.")
     coins = Coin.query.all()
+    
     return render_template("home.html", coins=coins)
 
 @app.route("/update", methods=["POST"])
@@ -103,29 +108,6 @@ def delete():
     return redirect("/")
 
 
-""" coin_name = input('What coin you want to search for?: ')
-initial_invesment = int(input('Enter your initial investment?: '))
-presale_price = float(input('Enter presale token price: '))
-
-coin_price = cg.get_price(ids=[coin_name.lower()], vs_currencies='usd')[coin_name.lower()]['usd']
-twenty_four_hour_change = cg.get_price(ids=[coin_name.lower()], vs_currencies='usd', include_24hr_change='true')[coin_name.lower()]['usd_24h_change']
-ath = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
-ath_change_percentage = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
-coin_image = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
-
-profit = round((initial_invesment / presale_price) * int(coin_price))
-multiplier = coin_price / presale_price
-
-print('Image: ',coin_image['image'])
-print('Coin: ', coin_name)
-print('Current Price: ',"${:,.2f}".format(int(coin_price)))
-print(f"Presale Price: ${presale_price}")
-print(f'Xs: {multiplier}x')
-print('Your Initial Investment Price: ',"${:,.2f}".format(initial_invesment))
-print('Your profit: ', "${:,.2f}".format(profit))
-print('24h: ',twenty_four_hour_change)
-print('ATH: ', "${:,.2f}".format(ath['ath']))
-print("% From ATH:", int(ath['ath_change_percentage'])) """
 
 if __name__ == "__main__":
     app.run(debug=True)
