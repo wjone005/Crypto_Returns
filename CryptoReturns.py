86a484570f7b809ece5a6e1f3e9b03f1ce668c63
Willie Jones III <williejones005@gmail.com>
2021-05-20 00:24:00 -0700

    Changed database string values to float, added dollar signs to dashboard and allowed to display two decimal places for dollar amounts

--- CryptoReturns.py
+++ CryptoReturns.py
@@ -12,6 +12,8 @@ from flask import flash
 
 from flask_sqlalchemy import SQLAlchemy
 
+from decimal import *
+
 cg = CoinGeckoAPI()
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -29,14 +31,14 @@ class Coin(db.Model):
 
     crypto_name = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     image = db.Column(db.String(80), unique=False, nullable=True)
-    current_price = db.Column(db.String(80), unique=False, nullable=True)
-    presale_price = db.Column(db.String(80), unique=False, nullable=True)
-    multiple = db.Column(db.String(80), unique=False, nullable=True)
-    coin_initial_invesment = db.Column(db.String(80), unique=False, nullable=True)
-    profit = db.Column(db.String(80), unique=False, nullable=True)
-    twenty_four_hour = db.Column(db.String(80), unique=False, nullable=True)
-    all_time_high = db.Column(db.String(80), unique=False, nullable=True)
-    all_time_percentage_change = db.Column(db.String(80), unique=False, nullable=True)
+    current_price = db.Column(db.Float(), unique=False, nullable=True)
+    presale_price = db.Column(db.Float(), unique=False, nullable=True)
+    multiple = db.Column(db.Float(), unique=False, nullable=True)
+    coin_initial_invesment = db.Column(db.Float(), unique=False, nullable=True)
+    profit = db.Column(db.Float(), unique=False, nullable=True)
+    twenty_four_hour = db.Column(db.Float(), unique=False, nullable=True)
+    all_time_high = db.Column(db.Float(), unique=False, nullable=True)
+    all_time_percentage_change = db.Column(db.Float(), unique=False, nullable=True)
     sparkline = db.Column(db.String(80), unique=False, nullable=True)
     def __repr__(self):
         return "<Crypto_name: {}>".format(self.crypto_name)
@@ -48,45 +50,48 @@ def home():
 
     if request.form:
         try:
-
+            
+            TWOPLACES = Decimal(10) ** -2
+            Decimal('3.214').quantize(TWOPLACES)
             user_input = request.form.get("coin")
             coin_name = user_input.lower().replace(" ", "-")
             coin_gecko_name = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
             initial_investment = request.form.get("initial_investment")
             coin_image = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
             coin_price = float(cg.get_price(ids=[coin_name.lower()], vs_currencies='usd')[coin_name.lower()]['usd'])
-            presale_price = request.form.get("presale_price")
-            multiplier = float(coin_price) / float(presale_price)
+            presale_price = float(request.form.get("presale_price"))
+            multiplier = Decimal(float(coin_price) / float(presale_price)).quantize(TWOPLACES)
             profit = round((float(initial_investment) / float(presale_price)) * float(coin_price))
-            twenty_four_hour_change = cg.get_price(ids=[coin_name.lower()], vs_currencies='usd', include_24hr_change='true')[coin_name.lower()]['usd_24h_change']
+            twenty_four_hour_change = Decimal(cg.get_price(ids=[coin_name.lower()], vs_currencies='usd', include_24hr_change='true')[coin_name.lower()]['usd_24h_change']).quantize(TWOPLACES)
             ath = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
             ath_change_percentage = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
+            print(ath_change_percentage)
             #coin_gecko_sparkline = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='true')[0]
-
+            print(type(ath_change_percentage))
             coin = Coin(
                         crypto_name=coin_gecko_name['name'],
                         coin_initial_invesment=initial_investment,
                         image=coin_image['image'],
-                        current_price="${:,.2f}".format(coin_price),
+                        current_price=coin_price,
                         presale_price=presale_price,
-                        multiple= "{:,.2f}".format(multiplier)+"x",
-                        profit="${:,.2f}".format(profit),
-                        twenty_four_hour="{:,.2f}".format(twenty_four_hour_change),
-                        all_time_high="${:,.2f}".format(ath['ath']),
-                        all_time_percentage_change="{:,.2f}".format(ath['ath_change_percentage']),
+                        multiple= multiplier,
+                        profit=profit,
+                        twenty_four_hour=twenty_four_hour_change,
+                        all_time_high=ath['ath'],
+                        all_time_percentage_change=Decimal(ath['ath_change_percentage']).quantize(TWOPLACES),
                         #sparkline=coin_gecko_sparkline['sparkline_in_7d']['price']
                         )
-            print(coin.crypto_name)
-            q = db.session.query(coin).filter(coin.name == coin_name)
+            #print(coin.crypto_name)
+            #q = db.session.query(coin).filter(coin.name == coin_name)
             #session.query(q.exists())
-            #db.session.add(coin)
-            #db.session.commit()
+            db.session.add(coin)
+            db.session.commit()
         except Exception as e:
             db.session.rollback()
             print('Failed to add coin')
             print(e)
-            print(coin_name)
-            flash(f"Could not find {coin_name}. Please enter {coin_name} full name.")
+            #print(coin_name)
+            #flash(f"Could not find {coin_name}. Please enter {coin_name} full name.")
     coins = Coin.query.all()
     
     return render_template("home.html", coins=coins)
@@ -96,8 +101,12 @@ def home():
 def update():
     newcrypto_name = request.form.get("newcrypto_name")
     oldcrypto_name = request.form.get("oldcrypto_name")
+    coin_name = newcrypto_name.lower().replace(" ", "-")
+    coin_gecko_name = cg.get_coins_markets(vs_currency='usd', ids=[coin_name.lower()], order='market_cap_desc', per_page='1', page='1', sparkline='false')[0]
+    
+    
     coin = Coin.query.filter_by(crypto_name=oldcrypto_name).first()
-    coin.crypto_name = newcrypto_name
+    coin.crypto_name = coin_gecko_name['name']
     db.session.commit()
     return redirect("/")
 
