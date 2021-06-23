6f5f81c1445fff243949dc3a758418a190ab5d89
Willie Jones III <williejones005@gmail.com>
2021-06-23 12:44:01 -0700

    added host to application.run

--- application.py
+++ application.py
@@ -2,4 +2,4 @@ from CryptoReturns import application
 
 # This is so we can run the application through python
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(debug=True, host='0.0.0.0')
